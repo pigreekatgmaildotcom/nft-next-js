73ae6471f9cb786328bb504654997a21084a49ea
Leonardi Marco <leonardim@WI0035950T.local>
2023-03-27 23:58:58 +0200

    changed directory structure

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from "next/head";
-import Header from "../../components/Header";
-import NFTMint from "../../components/NFTMint";
+import Header from "../components/Header";
+import NFTMint from "../components/NFTMint";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import styles from "../styles/Home.module.css";
 import { useMoralis } from "react-moralis";
 
 const inter = Inter({ subsets: ["latin"] });
