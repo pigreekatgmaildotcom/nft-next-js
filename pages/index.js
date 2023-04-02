eb636f6d68f57dc82c89e4b541f19b8bb91a8b13
Leonardi Marco <leonardim@WI0035950T.local>
2023-04-02 10:50:04 +0200

    Icona NFT

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         <title>Enneffetti</title>
         <meta name="description" content="Enneeffetti" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/NFTLogo.png" />
       </Head>
       <Header></Header>
       {isWeb3Enabled ? (
