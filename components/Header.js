0e9e41eab3971753e60cadfef06f83104920cbea
Leonardi Marco <leonardim@WI0035950T.local>
2023-04-06 09:48:20 +0200

    minor UI improvements

--- components/Header.js
+++ components/Header.js
@@ -5,27 +5,27 @@ export default function Header() {
   const { chainId } = useMoralis();
 
   var blockchainName = "";
-  var name = "";
+  var name = false;
 
   switch (parseInt(chainId)) {
     case 31337:
       blockchainName = "ethereum";
-      name = "Local";
+      name = true;
       break;
     case 5:
       /*blockchainName = "Goerli (Ethereum)";*/
       blockchainName = "ethereum";
-      name = "Goerli";
+      name = true;
       break;
     case 80001:
       /*blockchainName = "Mumbai (Polygon)";*/
       blockchainName = "polygon";
-      name = "Mumbai";
+      name = true;
       break;
     case 43113:
       /*blockchainName = "Fuji (Avalanche)";*/
       blockchainName = "avalanche";
-      name = "Fuji";
+      name = true;
       break;
     default:
       blockchainName = "ethereum";
@@ -39,16 +39,19 @@ export default function Header() {
 
   return (
     <div className="shadow-lg shadow-black/40 flex justify-between bg-gradient-to-r from-cyan-500 to-blue-500">
-      <h1>
-        <div className="py-2 px-4 flex">
+      <div className="py-2 px-4 flex">
+        {name ? (
           <CryptoLogos
             chain={blockchainName}
             onClick={function noRefCheck() {}}
             size="40px"
           />
-          <div className="px-3 py-1 font-blog text-xl text-white">{name}</div>
-        </div>
-      </h1>
+        ) : (
+          <div className="py-1 px-2 font-blog text-lg text-white">
+            Enneeffeti
+          </div>
+        )}
+      </div>
       <div className="py-2 px-4">
         <ConnectButton moralisAuth={false} />
       </div>
